217c00cbf72dc4a024b3c41fa6b88693e7676006
Miquel Delgado <Mikiodelg@gmail.com>
2014-03-04 09:48:16 +0100

    Entrega JDBC y MySQL. v1.0

---
+++ DB1.sql
@@ -0,0 +1,18 @@
+
+DROP DATABASE IF EXISTS DB1;
+CREATE DATABASE DB1;
+USE DB1;
+CREATE TABLE Manufacturers (
+  Code INTEGER,
+  Name VARCHAR(255) NOT NULL,
+  PRIMARY KEY (Code)   
+);
+
+CREATE TABLE Products (
+  Code INTEGER,
+  Name VARCHAR(255) NOT NULL ,
+  Price DECIMAL NOT NULL ,
+  Manufacturer INTEGER NOT NULL,
+  PRIMARY KEY (Code), 
+  FOREIGN KEY (Manufacturer) REFERENCES Manufacturers(Code)
+) ENGINE=INNODB;
